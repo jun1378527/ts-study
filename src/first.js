3f2675fde0af194d1854b1ec25e12128466cad5b
jun1378527 <81806934+jun1378527@users.noreply.github.com>
2024-08-13 22:39:21 +0900

    타입스크립트의 기초

---
+++ src/first.js
@@ -0,0 +1,8 @@
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(5, 10)); // 15
+console.log(add(5, "10")); // "510" (문자열로 반환됨)
+console.log(add("hello", "world")); //"helloworld"
+console.log(add(3, 4, 5)); // 7
---
+++ src/first.ts
@@ -0,0 +1,11 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+// 올바른 사용 예시
+console.log(add(5, 10)); // 15
+
+// 오류 발생 예시들 (타입스크립트에서 컴파일 오류 발생)
+console.log(add(5, "10")); // 오류: 'string' 형식의 인수는 'number' 형식의 매개 변수에 할당될 수 없습니다..
+console.log(add("hello", "world")); // 오류: 'string' 형식의 인수는 'number' 형식의 매개 변수에 할당될 수 없습니다..
+console.log(add(3, 4, 5)); // 오류 : 2개의 인수가 필요한데 3개를 가져왔습니다.
